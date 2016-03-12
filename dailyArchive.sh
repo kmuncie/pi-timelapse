4cb02e297719d38387361b691aea59035939874c
Kevin Muncie <nivek2002@gmail.com>
2016-03-11 22:25:43 -0500

    Make smarter to deal with long upload times

--- dailyArchive.sh
+++ dailyArchive.sh
@@ -2,4 +2,14 @@
 
 TODAY=$(date +"%Y-%m-%d")
 
-aws s3 mv ~/webcam/images/today/ s3://pi-timelapse-archive/$TODAY/ --recursive --exclude '*' --include '*.jpg'
+# Move days images to temp folder so we can do long upload
+cp -avr ~/webcam/images/today ~/webcam/images/todayTEMP
+
+# Delete days images immediately after move so new photos can start at 0001
+rm ~/webcam/image/today/*.jpg
+
+# Upload to s3 bucket from temp folder
+aws s3 mv ~/webcam/images/todayTEMP/ s3://pi-timelapse-archive/$TODAY/ --recursive --exclude '*' --include '*.jpg'
+
+# Delete temp folder after upload
+rm -rf ~/webcam/images/todayTEMP
